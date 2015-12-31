668619c9dcfad0558e2576d9431510806af71fb7
Karácsony Máté <k_mate@inf.elte.hu>
2015-12-31 15:16:47 +0100

    Placing player buttons

--- content.js
+++ content.js
@@ -7,29 +7,68 @@ var observer = new MutationObserver(function(mutations) {
         handleChange();
       }
     }
-  })
+  });
 });
 observer.observe(document, { childList: true, subtree: true });
 
+var keyboardObserver;
+var speakerObserver;
+
 function handleChange() {
-  // Text translation from Swedish to English
-  var keyboard = $('div#vkeyboard_keys_1');
-  if (0 < keyboard.length) {
-    addPlayerToKeyboard(keyboard);
-  }
+  cleanupObservers();
 
   // Text translation from English to Swedish
-  var speaker = $('span.speaker-small');
-  console.log(speaker);
-  console.log(speaker.length);
-  if (0 < speaker.length) {
-    addPlayerToSpeaker(speaker);
-    return;
+  setupKeyboardPlayer();
+
+  // Text translation from Swedish to English
+  setupSpeakerPlayer();
+}
+
+function cleanupObservers() {
+  if (!!keyboardObserver) {
+    console.debug('removing keyboard observer');
+    keyboardObserver.disconnect();
+    keyboardObserver = null;
+  }
+
+  if (!!speakerObserver) {
+    console.debug('removing speaker observer');
+    speakerObserver.disconnect();
+    speakerObserver = null;
   }
 }
 
+function setupKeyboardPlayer() {
+  var keyboard = $('div#vkeyboard_keys_1').last();
+  if (0 == keyboard.length) {
+    // nowhere to place the button
+    return;
+  }
+
+  addPlayerToKeyboard(keyboard);
+
+  // the keyboard changes when the user types
+  var placeholder = keyboard.parent().parent();
+  keyboardObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (0 < mutation.addedNodes.length) {
+        // we need to look up the new keyboard here
+        keyboard = $('div#vkeyboard_keys_1');
+        addPlayerToKeyboard(keyboard);
+      }
+    });
+  });
+  keyboardObserver.observe(placeholder.get(0), { childList: true });
+}
+
 function addPlayerToKeyboard(keyboard) {
+  if (0 < keyboard.children('.forvo-player').length) {
+    // do not add the button twice
+    return;
+  }
+
   var img = document.createElement('img');
+  img.classList.add('forvo-player');
   img.classList.add('vkeyboard_key');
   img.classList.add('column');
   img.src = chrome.extension.getURL('forvo-button.png');
@@ -37,16 +76,48 @@ function addPlayerToKeyboard(keyboard) {
   keyboard.prepend(img);
 }
 
+function setupSpeakerPlayer() {
+  var speaker = $('span#big-speaker').last();
+  if (0 == speaker.length) {
+    // nowhere to place the button
+    return;
+  }
+
+  addPlayerToSpeaker(speaker);
+
+  speakerObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (0 < mutation.addedNodes.length) {
+        addPlayerToSpeaker(speaker);
+      }
+    });
+  });
+  speakerObserver.observe(speaker.get(0), { childList: true });
+}
+
 function addPlayerToSpeaker(speaker) {
-  console.log(speaker);
-  var span = document.createElement('span'); 
-  span.classList.add('icon');
-  span.css({ left: '34px', top: 0 });
+  if (0 < speaker.children('.forvo-player').length) {
+    // do not add the button twice
+    return;
+  }
+
+  if (0 == speaker.children().length) {
+    // the original speaker is not added yet
+    return;
+  }
+
+  speaker.children().first().css({ marginRight: '7px' });
+  var span = document.createElement('span');
+  span.classList.add('forvo-player');
+  span.classList.add('speaker-small');
+  span.style.left = '7px';
+  span.style.marginRight = '14px';
   var img = document.createElement('img');
   img.src = chrome.extension.getURL('forvo-button.png');
   img.title = 'Pronounce with Forvo';
+  img.style.width = '30px';
+  img.style.height = '30px';
   span.appendChild(img);
-  speaker.css({ marginRight: '37px' });
   speaker.append(span);
 }
 
